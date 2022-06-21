6bdc063242f28e04ae51bad0b17d2a2525e3a7bd
JulianEstebanRamirezCordoba <julianestebanramirezcordoba25@gmail.com>
2022-06-21 16:33:41 -0500

    No se sabe que se agrega

---
+++ templates_c/969bb506aa8fa7f3d5fb391b090238bdf2345b02_0.file.index.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 3.1.38, created on 2022-06-21 22:34:23
+  from 'C:\xampp\htdocs\smarty-crud\templates\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.38',
+  'unifunc' => 'content_62b22b4f822a30_64914268',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '969bb506aa8fa7f3d5fb391b090238bdf2345b02' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\smarty-crud\\templates\\index.tpl',
+      1 => 1655843567,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_62b22b4f822a30_64914268 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+    <head>
+        <title>Pagina</title>
+    </head>
+    <body>
+    <?php echo $_smarty_tpl->tpl_vars['mensaje']->value;?>
+
+    </body>
+</html><?php }
+}
