97abb5e2a56349fe70ca820cce862d263fb48c86
d1lanX <rodriguezdylan58@gmail.com>
2022-06-21 13:48:25 -0500

    Commit inicial, conexion a base de datos

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+
+define('DB_HOST', 'mysql-udevo.alwaysdata.net');
+define('DB_USER', 'udevo');
+define('DB_PASS', 'cliperino');
+define('DB_NAME', 'udevo_empleados');
+
+try
+{
+
+    $connect = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS,
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+}
+catch(PDOException $e)
+{
+    exit("Error: ". $e->getMessage());
+}
+
+?>
