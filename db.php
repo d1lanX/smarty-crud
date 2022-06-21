4693b92e773b44efa23598956ac20595ca61260e
d1lanX <rodriguezdylan58@gmail.com>
2022-06-21 13:55:48 -0500

    Agregue las carpetas css, js, php

--- db.php
+++ db.php
@@ -5,9 +5,7 @@ define('DB_USER', 'udevo');
 define('DB_PASS', 'cliperino');
 define('DB_NAME', 'udevo_empleados');
 
-try
-{
-
+try{ 
     $connect = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS,
     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
