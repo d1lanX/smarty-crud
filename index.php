8d86c6e663a1d12e615e5c06e7b414b8b394c14d
d1lanX <rodriguezdylan58@gmail.com>
2022-06-21 14:54:16 -0500

    Agregue la configuracion de smarty

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    require('Smarty.class.php');
+    $smarty = new Smarty;
+    $smarty->template_dir = './templates';
+    $smarty->config_dir = './config';
+    $smarty->cache_dir = './cache';
+    $smarty->compile_dir = './templates_c';
+    $smarty->assign('mensaje', 'Bienvenidos!');
+    $smarty->display('index.tpl');
+
+
+?>
