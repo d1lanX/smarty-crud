e7086650f74d179e2b1ba478dc2f9c4537e68fbd
d1lanX <rodriguezdylan58@gmail.com>
2022-06-22 09:11:26 -0500

    Se mejora el formateo de lineas en footer.tpl

--- templates/footer.tpl
+++ templates/footer.tpl
@@ -1,2 +1,15 @@
-<footer>2022</footer>
-</html>
+{* inicio footer *}
+<div class="container">
+    <div class="card">
+    <div class="card-header">
+        Frase
+    </div>
+    <div class="card-body">
+        <blockquote class="blockquote mb-0">
+        <p>La forma de empezar es dejar de hablar y empezar a hacer.</p>
+        <footer class="blockquote-footer">Walt-Disney</footer>
+        </blockquote>
+    </div>
+    </div>
+</div>
+{* fin html  *}
--- templates_c/168c34fe731aec7b3a2d12ae29abd995176d1ce2_0.file.footer.tpl.php
+++ templates_c/168c34fe731aec7b3a2d12ae29abd995176d1ce2_0.file.footer.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.38, created on 2022-06-21 23:10:14
+/* Smarty version 3.1.38, created on 2022-06-21 23:27:37
   from 'D:\Xampp\htdocs\smarty-crud\templates\footer.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.38',
-  'unifunc' => 'content_62b233b61a9f04_45009212',
+  'unifunc' => 'content_62b237c948a468_29871764',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '168c34fe731aec7b3a2d12ae29abd995176d1ce2' => 
     array (
       0 => 'D:\\Xampp\\htdocs\\smarty-crud\\templates\\footer.tpl',
-      1 => 1655845665,
+      1 => 1655846855,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,22 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_62b233b61a9f04_45009212 (Smarty_Internal_Template $_smarty_tpl) {
-?><footer>2022</footer>
-</html><?php }
+function content_62b237c948a468_29871764 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="container">
+
+<div class="card">
+  <div class="card-header">
+    Frase
+  </div>
+  <div class="card-body">
+    <blockquote class="blockquote mb-0">
+      <p>La forma de empezar es dejar de hablar y empezar a hacer.</p>
+      <footer class="blockquote-footer">Walt-Disney</footer>
+    </blockquote>
+  </div>
+</div>
+
+</div>
+ <?php }
 }
