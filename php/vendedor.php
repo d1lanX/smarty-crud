01a86e79c18192da2a0517a5266d39652952f9a9
JulianEstebanRamirezCordoba <julianestebanramirezcordoba25@gmail.com>
2022-06-23 00:17:28 -0500

    Se empieza a realizar las consultas de datos

--- php/vendedor.php
+++ php/vendedor.php
@@ -1,9 +1,27 @@
 <?php 
+    include_once("../db.php");
     require('Smarty.class.php');
+
     $smarty = new Smarty;
     $smarty->template_dir = '../templates';
     $smarty->config_dir = '../config';
     $smarty->cache_dir = '../cache';
     $smarty->compile_dir = '../templates_c';
+
+    function consultaSelect($nombreTabla, $camposConsult, $valoresConsult){
+        global $connect;
+        $condicion = "WHERE";
+
+        for($index = 0; $index < $camposConsult; $index++){
+            $condicion = ($index == (count($camposConsult) - 1)) ? $condicion."`". $camposConsult[$index]."` = '"
+                .$valoresConsult[$index]."'" : $condicion."`".$camposConsult[$index]."`= '".$valoresConsult[$index].
+                "' AND ";
+
+        }
+
+        $sql = "";
+
+    }
+
     $smarty->display('vendedor.tpl');
 ?>
